83957ea4acb1db9d475393b3e9f1bfb6ba8ba181
udos86 <git@udos86.de>
2015-03-25 21:29:24 +0100

    new files added

---
+++ app/src/main/java/de/udos/democardlist/CardViewItemAdapter.java
@@ -0,0 +1,48 @@
+package de.udos.democardlist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class CardViewItemAdapter extends RecyclerView.Adapter<CardViewItemAdapter.ViewHolder> {
+
+    private String[] mData;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTextView;
+
+        public ViewHolder(View v) {
+
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.text_view);
+        }
+    }
+
+    public CardViewItemAdapter(String[] data) {
+        mData = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemRootView = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.recycler_view_item, parent, false);
+
+        return new ViewHolder(itemRootView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mData[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.length;
+    }
+
+}
--- app/src/main/java/de/udos/democardlist/MainActivity.java
+++ app/src/main/java/de/udos/democardlist/MainActivity.java
@@ -1,17 +1,32 @@
 package de.udos.democardlist;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new CardViewFragment())
+                    .commit();
+        }
     }
 
 
@@ -35,5 +50,34 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class CardViewFragment extends Fragment {
+
+        private RecyclerView mRecyclerView;
+        private RecyclerView.Adapter mAdapter;
+        private RecyclerView.LayoutManager mLayoutManager;
+
+        public CardViewFragment() {
+            // leerer Konstruktor per Definition
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+            mLayoutManager = new LinearLayoutManager(getActivity());
+            mAdapter = new CardViewItemAdapter(new String[] {"TEST1", "TEST2"});
+
+            mRecyclerView.setHasFixedSize(true);
+            mRecyclerView.setLayoutManager(mLayoutManager);
+            mRecyclerView.setAdapter(mAdapter);
+
+            return rootView;
+        }
     }
 }
